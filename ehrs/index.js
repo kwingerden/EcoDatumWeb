850ad5e9b7e876bc5a1fd39a9747656a6f513b9e
Ken Wingerden <ken.wingerden@gmail.com>
2019-04-23 21:06:37 -0700

    added ERHS tree data

---
+++ ehrs/index.js
@@ -0,0 +1,677 @@
+'use strict';
+
+const mapInfo = {
+    mapCenter: {
+        lat: 34.883910,
+        lng: -120.421557
+    },
+    mapType: 'satellite',
+    startZoom: 20
+};
+
+let siteData = [
+    {
+        title: 'Tree 1',
+        location: {
+            lat: 34.883979,
+            lng: -120.422244
+        },
+        images: [
+            {
+                title: 'Tree 1',
+                url: 'site1/2.png',
+                date: '2018-05-30'
+            }
+        ],
+        notes: [
+
+        ],
+        data: [
+            {
+                date: '2018-05-30',
+                factor: 'Location',
+                type: 'Latitude',
+                value: '34.883979',
+                unit: '°'
+            },
+            {
+                date: '2018-05-30',
+                factor: 'Location',
+                type: 'Longitude',
+                value: '-120.422244',
+                unit: '°'
+            },
+            {
+                date: '2018-05-30',
+                factor: 'Biotic',
+                type: 'Carbon',
+                value: '33',
+                unit: 'kg'
+            }
+        ]
+    },
+    {
+        title: 'Tree 2',
+        location: {
+            lat: 34.884032,
+            lng: -120.421759
+        },
+        images: [
+            {
+                title: 'Tree 2',
+                url: 'site2/2.png',
+                date: '2018-05-30'
+            }
+        ],
+        notes: [
+
+        ],
+        data: [
+            {
+                date: '2018-05-30',
+                factor: 'Location',
+                type: 'Latitude',
+                value: '34.884032',
+                unit: '°'
+            },
+            {
+                date: '2018-05-30',
+                factor: 'Location',
+                type: 'Longitude',
+                value: '-120.421759',
+                unit: '°'
+            },
+            {
+                date: '2018-05-30',
+                factor: 'Biotic',
+                type: 'Carbon',
+                value: '139',
+                unit: 'kg'
+            }
+        ]
+    },
+    {
+        title: 'Tree 3',
+        location: {
+            lat: 34.884147,
+            lng: -120.421879
+        },
+        images: [
+            {
+                title: 'Tree 3',
+                url: 'site2/3.png',
+                date: '2018-05-30'
+            }
+        ],
+        notes: [
+
+        ],
+        data: [
+            {
+                date: '2018-05-30',
+                factor: 'Location',
+                type: 'Latitude',
+                value: '34.884147',
+                unit: '°'
+            },
+            {
+                date: '2018-05-30',
+                factor: 'Location',
+                type: 'Longitude',
+                value: '-120.421879',
+                unit: '°'
+            },
+            {
+                date: '2018-05-30',
+                factor: 'Biotic',
+                type: 'Carbon',
+                value: '276',
+                unit: 'kg'
+            }
+        ]
+    },
+    {
+        title: 'Tree 4',
+        location: {
+            lat: 34.884177,
+            lng: -120.421666
+        },
+        images: [
+            {
+                title: 'Tree 4',
+                url: 'site4/2.png',
+                date: '2018-05-30'
+            }
+        ],
+        notes: [
+
+        ],
+        data: [
+            {
+                date: '2018-05-30',
+                factor: 'Location',
+                type: 'Latitude',
+                value: '34.884177',
+                unit: '°'
+            },
+            {
+                date: '2018-05-30',
+                factor: 'Location',
+                type: 'Longitude',
+                value: '-120.421666',
+                unit: '°'
+            },
+            {
+                date: '2018-05-30',
+                factor: 'Biotic',
+                type: 'Carbon',
+                value: '124',
+                unit: 'kg'
+            }
+        ]
+    },
+    {
+        title: 'Tree 5',
+        location: {
+            lat: 34.884130,
+            lng: -120.421577
+        },
+        images: [
+            {
+                title: 'Tree 5',
+                url: 'site2/4.png',
+                date: '2018-05-30'
+            }
+        ],
+        notes: [
+
+        ],
+        data: [
+            {
+                date: '2018-05-30',
+                factor: 'Location',
+                type: 'Latitude',
+                value: '34.884130',
+                unit: '°'
+            },
+            {
+                date: '2018-05-30',
+                factor: 'Location',
+                type: 'Longitude',
+                value: '-120.421577',
+                unit: '°'
+            },
+            {
+                date: '2018-05-30',
+                factor: 'Biotic',
+                type: 'Carbon',
+                value: '67',
+                unit: 'kg'
+            }
+        ]
+    },
+    {
+        title: 'Tree 6',
+        location: {
+            lat: 34.884085,
+            lng: -120.421600
+        },
+        images: [
+            {
+                title: 'Tree 6',
+                url: 'site2/6.png',
+                date: '2018-05-30'
+            }
+        ],
+        notes: [
+
+        ],
+        data: [
+            {
+                date: '2018-05-30',
+                factor: 'Location',
+                type: 'Latitude',
+                value: '34.884085',
+                unit: '°'
+            },
+            {
+                date: '2018-05-30',
+                factor: 'Location',
+                type: 'Longitude',
+                value: '-120.421600',
+                unit: '°'
+            },
+            {
+                date: '2018-05-30',
+                factor: 'Biotic',
+                type: 'Carbon',
+                value: '33',
+                unit: 'kg'
+            }
+        ]
+    },
+    {
+        title: 'Tree 7',
+        location: {
+            lat: 34.884036,
+            lng: -120.421627
+        },
+        images: [
+            {
+                title: 'Tree 7',
+                url: 'site2/5.png',
+                date: '2018-05-30'
+            }
+        ],
+        notes: [
+
+        ],
+        data: [
+            {
+                date: '2018-05-30',
+                factor: 'Location',
+                type: 'Latitude',
+                value: '34.884036',
+                unit: '°'
+            },
+            {
+                date: '2018-05-30',
+                factor: 'Location',
+                type: 'Longitude',
+                value: '-120.421627',
+                unit: '°'
+            },
+            {
+                date: '2018-05-30',
+                factor: 'Biotic',
+                type: 'Carbon',
+                value: '24',
+                unit: 'kg'
+            }
+        ]
+    },
+    {
+        title: 'Tree 8',
+        location: {
+            lat: 34.883844,
+            lng: -120.421487
+        },
+        images: [
+            {
+                title: 'Tree 8',
+                url: 'site3/2.png',
+                date: '2018-05-30'
+            }
+        ],
+        notes: [
+
+        ],
+        data: [
+            {
+                date: '2018-05-30',
+                factor: 'Location',
+                type: 'Latitude',
+                value: '34.883844',
+                unit: '°'
+            },
+            {
+                date: '2018-05-30',
+                factor: 'Location',
+                type: 'Longitude',
+                value: '-120.421487',
+                unit: '°'
+            },
+            {
+                date: '2018-05-30',
+                factor: 'Biotic',
+                type: 'Carbon',
+                value: '132',
+                unit: 'kg'
+            }
+        ]
+    },
+    {
+        title: 'Tree 9',
+        location: {
+            lat: 34.883953,
+            lng: -120.421279
+        },
+        images: [
+            {
+                title: 'Tree 9',
+                url: 'site3/4.png',
+                date: '2018-05-30'
+            }
+        ],
+        notes: [
+
+        ],
+        data: [
+            {
+                date: '2018-05-30',
+                factor: 'Location',
+                type: 'Latitude',
+                value: '34.883953',
+                unit: '°'
+            },
+            {
+                date: '2018-05-30',
+                factor: 'Location',
+                type: 'Longitude',
+                value: '-120.421279',
+                unit: '°'
+            },
+            {
+                date: '2018-05-30',
+                factor: 'Biotic',
+                type: 'Carbon',
+                value: '25',
+                unit: 'kg'
+            }
+        ]
+    },
+    {
+        title: 'Tree 10',
+        location: {
+            lat: 34.883964,
+            lng: -120.421024
+        },
+        images: [
+            {
+                title: 'Tree 10',
+                url: 'site3/3.png',
+                date: '2018-05-30'
+            }
+        ],
+        notes: [
+
+        ],
+        data: [
+            {
+                date: '2018-05-30',
+                factor: 'Location',
+                type: 'Latitude',
+                value: '34.883964',
+                unit: '°'
+            },
+            {
+                date: '2018-05-30',
+                factor: 'Location',
+                type: 'Longitude',
+                value: '-120.421024',
+                unit: '°'
+            },
+            {
+                date: '2018-05-30',
+                factor: 'Biotic',
+                type: 'Carbon',
+                value: '57',
+                unit: 'kg'
+            }
+        ]
+    }
+];
+
+const mapElement = document.getElementById('map');
+const sitesElement = document.getElementById('sites');
+
+const map = new google.maps.Map(
+    mapElement,
+    {
+        center: mapInfo.mapCenter,
+        mapTypeId: mapInfo.mapType,
+        zoom: mapInfo.startZoom,
+        tilt: 45,
+        fullscreenControl: true,
+        mapTypeControl: false,
+        panControl: true,
+        rotateControl: true,
+        scaleControl: true,
+        streetViewControl: false,
+        zoomControl: true
+    });
+
+const markers = [];
+
+function setSelectedMarker(marker, scroll = true) {
+    markers.forEach(function (marker, index) {
+        marker.infoWindow.close();
+        let siteIndex = index + 1;
+        marker.setZIndex(siteIndex);
+        let rowElement = document.getElementById('site-row-' + siteIndex);
+        rowElement.style.background = 'white';
+    });
+    let rowElement = document.getElementById('site-row-' + marker.zIndex);
+    rowElement.style.background = 'lightgray';
+    if (scroll) {
+        rowElement.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+    marker.infoWindow.open(map, marker);
+    map.panTo(marker.siteData.location);
+    marker.setZIndex(100);
+}
+
+function showSiteDetails(marker) {
+    let dataCarousel = document.getElementById('data-carousel');
+    dataCarousel.innerHTML = '';
+
+    let owlCarouselDiv = document.createElement('div');
+    owlCarouselDiv.className = 'owl-carousel owl-theme';
+    dataCarousel.appendChild(owlCarouselDiv);
+
+    marker.siteData.images.forEach(function (img) {
+        /*
+        let titleElement = document.createElement('p');
+        titleElement.style.textAlign = 'center';
+        titleElement.style.fontSize = '16px';
+        titleElement.innerText = img.title == null ? ' ' : img.title;
+        */
+
+        let imgElement = document.createElement('img');
+        imgElement.src = img.url;
+
+        let dateLabel = document.createElement('p');
+        dateLabel.style.textAlign = 'center';
+        dateLabel.style.fontSize = '16px';
+        dateLabel.innerText = img.date;
+
+        let itemDiv = document.createElement('div');
+        itemDiv.className = 'item';
+        //itemDiv.appendChild(titleElement);
+        itemDiv.appendChild(imgElement);
+        itemDiv.appendChild(dateLabel);
+
+        owlCarouselDiv.appendChild(itemDiv);
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop: false,
+        margin: 10,
+        nav: true,
+        autoHeight: false,
+        center: false,
+        items: 3
+    });
+
+    /*
+    let dataNotesText = document.getElementById('data-notes-text');
+    dataNotesText.value = '';
+    marker.siteData.notes.forEach(function (note, index) {
+        if (index === 0) {
+            dataNotesText.value = note;
+        } else {
+            dataNotesText.value = dataNotesText.value + '\n' + note;
+        }
+
+    });
+    */
+
+    let dataTableDiv = document.getElementById('data-table-div');
+    dataTableDiv.innerHTML = '';
+
+    let dataTable = document.createElement('table');
+    dataTable.id = 'data-table';
+    dataTable.className = 'display';
+    dataTable.style.width = '100%';
+
+    let dataTableHeader = document.createElement('thead');
+    let dataTableHeaderRow = document.createElement('tr');
+    let dataTableHeaderDate = document.createElement('th');
+    dataTableHeaderDate.innerText = 'Date';
+    let dataTableHeaderFactor = document.createElement('th');
+    dataTableHeaderFactor.innerText = 'Factor';
+    let dataTableHeaderType = document.createElement('th');
+    dataTableHeaderType.innerText = 'Type';
+    let dataTableHeaderValue = document.createElement('th');
+    dataTableHeaderValue.innerText = 'Value';
+    let dataTableHeaderUnit = document.createElement('th');
+    dataTableHeaderUnit.innerText = 'Unit';
+    dataTableHeaderRow.appendChild(dataTableHeaderDate);
+    dataTableHeaderRow.appendChild(dataTableHeaderFactor);
+    dataTableHeaderRow.appendChild(dataTableHeaderType);
+    dataTableHeaderRow.appendChild(dataTableHeaderValue);
+    dataTableHeaderRow.appendChild(dataTableHeaderUnit);
+    dataTableHeader.appendChild(dataTableHeaderRow);
+
+    let dataTableBody = document.createElement('tbody');
+
+    let dataTableFooter = document.createElement('tfoot');
+    let dataTableFooterRow = document.createElement('tr');
+    let dataTableFooterDate = document.createElement('th');
+    dataTableFooterDate.innerText = 'Date';
+    let dataTableFooterFactor = document.createElement('th');
+    dataTableFooterFactor.innerText = 'Factor';
+    let dataTableFooterType = document.createElement('th');
+    dataTableFooterType.innerText = 'Type';
+    let dataTableFooterValue = document.createElement('th');
+    dataTableFooterValue.innerText = 'Value';
+    let dataTableFooterUnit = document.createElement('th');
+    dataTableFooterUnit.innerText = 'Unit';
+    dataTableFooterRow.appendChild(dataTableFooterDate);
+    dataTableFooterRow.appendChild(dataTableFooterFactor);
+    dataTableFooterRow.appendChild(dataTableFooterType);
+    dataTableFooterRow.appendChild(dataTableFooterValue);
+    dataTableFooterRow.appendChild(dataTableFooterUnit);
+    dataTableFooter.appendChild(dataTableFooterRow);
+
+    dataTable.appendChild(dataTableHeader);
+    dataTable.appendChild(dataTableBody);
+    dataTable.appendChild(dataTableFooter);
+
+    dataTableDiv.appendChild(dataTable);
+
+    if (marker.siteData.data.length > 0) {
+        marker.siteData.data.forEach(function (data) {
+            let dateColumn = document.createElement('td');
+            dateColumn.innerText = data.date;
+
+            let factorColumn = document.createElement('td');
+            factorColumn.innerText = data.factor;
+
+            let typeColumn = document.createElement('td');
+            typeColumn.innerText = data.type;
+
+            let valueColumn = document.createElement('td');
+            valueColumn.innerText = data.value;
+
+            let unitColumn = document.createElement('td');
+            unitColumn.innerText = data.unit;
+
+            let tableRow = document.createElement('tr');
+            tableRow.style.textAlign = 'center';
+            tableRow.appendChild(dateColumn);
+            tableRow.appendChild(factorColumn);
+            tableRow.appendChild(typeColumn);
+            tableRow.appendChild(valueColumn);
+            tableRow.appendChild(unitColumn);
+
+            dataTableBody.appendChild(tableRow);
+        });
+    }
+    $('#data-table').DataTable();
+
+    $('#site-data-modal').dialog({
+        title: marker.siteData.title,
+        modal: true,
+        width: $(window).width() - 100,
+        height: $(window).height() - 100
+    });
+    let topElement = document.getElementById('top');
+    topElement.scrollIntoView();
+}
+
+siteData.forEach(function (siteData, index) {
+    let siteIndex = index + 1;
+
+    let marker = new google.maps.Marker({
+        position: new google.maps.LatLng(siteData.location.lat, siteData.location.lng),
+        title: siteData.title,
+        label: siteIndex.toString(),
+        map: map,
+        animation: google.maps.Animation.DROP,
+        optimized: false,
+        zIndex: siteIndex
+    });
+    markers.push(marker);
+
+    marker.siteData = siteData;
+
+    let titleElement = document.createElement('a');
+    titleElement.href = '#';
+    titleElement.innerText = siteData.title;
+    titleElement.onclick = function() {
+        showSiteDetails(marker);
+    };
+    marker.infoWindow = new google.maps.InfoWindow({
+        content: titleElement
+    });
+    marker.addListener('click', function () {
+        setSelectedMarker(marker);
+    });
+
+    let siteRow = document.getElementById('site-row-' + siteIndex);
+    siteRow.ondblclick = function () {
+        setSelectedMarker(marker, false);
+        showSiteDetails(marker);
+    };
+
+    let siteTitle = document.getElementById('site-title-' + siteIndex);
+    siteTitle.innerText = siteIndex + '. ' + siteData.title;
+    siteTitle.onclick = function () {
+        setSelectedMarker(marker, false);
+    };
+    siteTitle.ondblclick = function () {
+        showSiteDetails(marker);
+    };
+
+    let siteImage = document.getElementById('site-image-' + siteIndex);
+    siteImage.src = siteData.images[0].url;
+    siteImage.style.cursor = 'pointer';
+    siteImage.onmouseover = function () {
+        siteImage.style.borderColor = 'blue';
+        siteImage.style.borderStyle = 'solid';
+        siteImage.style.borderWidth = 'thin';
+    };
+    siteImage.onmouseout = function () {
+        siteImage.style.border = '';
+    };
+    siteImage.onclick = function () {
+        setSelectedMarker(marker, false);
+    };
+    siteImage.ondblclick = function () {
+        showSiteDetails(marker);
+    };
+});
+
+function updateElementHeights() {
+    let height = window.innerHeight ||
+        document.documentElement.clientHeight ||
+        document.body.clientHeight;
+    sitesElement.style.height = height + 'px';
+    mapElement.style.height = height + 'px';
+}
+
+window.onload = updateElementHeights;
+window.onresize = updateElementHeights;
